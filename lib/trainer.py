a1c42a1f8889edcdb35b15a58736f1e7368f99e6
jasonjabbour <55008744+jasonjabbour@users.noreply.github.com>
2022-11-16 14:50:05 -0500

    return prediction of node model

--- lib/trainer.py
+++ lib/trainer.py
@@ -148,6 +148,16 @@ class Trainer(nn.Module):
             error_rate = ((y_test - prediction) ** 2).mean()
         return error_rate
     
+    def evaluate_mse_pred(self, X_test, y_test, device, batch_size=4096):
+        X_test = torch.as_tensor(X_test, device=device)
+        y_test = check_numpy(y_test)
+        self.model.train(False)
+        with torch.no_grad():
+            prediction = process_in_chunks(self.model, X_test, batch_size=batch_size)
+            prediction = check_numpy(prediction)
+            error_rate = ((y_test - prediction) ** 2).mean()
+        return error_rate, prediction
+    
     def evaluate_auc(self, X_test, y_test, device, batch_size=512):
         X_test = torch.as_tensor(X_test, device=device)
         y_test = check_numpy(y_test)
